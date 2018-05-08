6c5abbff7334de23431d5f992addb6753fa591a5
UladzislauCharneny <vladislav.chernyony@gmail.com>
2018-05-08 23:11:06 +0200

    Add yarn start

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -2,4 +2,23 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.plugins.append(
+  'CommonsChunkVendor',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    minChunks: (module) => {
+      // this assumes your vendor imports exist in the node_modules directory
+      return module.context && module.context.indexOf('node_modules') !== -1
+    }
+  })
+)
+
+environment.plugins.append(
+  'CommonsChunkManifest',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'manifest',
+    minChunks: Infinity,
+  })
+);
+
 module.exports = environment.toWebpackConfig()
