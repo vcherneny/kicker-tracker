2ff3b0d0731931aba7938f8f22e2c8a4d6bcc4ae
Vlad Ragunovich <vlad.ragunovich@gmail.com>
2018-05-09 10:44:21 +0200

    Add games/current api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'application#index'
-  get '*path', to: 'application#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'score/left', to: 'scores#left'
   post 'score/right', to: 'scores#right'
-  resources :games
+  resources :games do
+    get :current, on: :collection
+  end
+
+  get '*path', to: 'application#index'
 end
