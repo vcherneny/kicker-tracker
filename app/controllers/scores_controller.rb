d91a6dac56374eafff76a79572111d45690d5a32
Vlad Ragunovich <vlad.ragunovich@gmail.com>
2018-05-09 15:04:14 +0200

    Fix scores

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,17 +1,19 @@
 class ScoresController < ApplicationController
-  before_action :set_current_game
+  # before_action :set_current_game
 
   def left
-    @current_game.increment!(:left_team_score) if @current_game.present?
+    left_team_score = current_game.left_team_score + 1
+    current_game.update(left_team_score: right_team_score) if current_game.present?
   end
 
   def right
-    @current_game.increment!(:right_team_score) if @current_game.present?
+    right_team_score = current_game.right_team_score + 1
+    current_game.update(right_team_score: right_team_score) if current_game.present?
   end
 
   private
 
-  def set_current_game
-    @current_game = Game.current
+  def current_game
+    @current_game ||= Game.current
   end
 end
